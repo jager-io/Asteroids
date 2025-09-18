842ad2d180ccc48fec735d43dd27b959fbd45be5
jager-io <jagerwilloughby@gmail.com>
2025-09-19 06:24:26 +0800

    Added screen

--- main.py
+++ main.py
@@ -15,8 +15,14 @@ def main():
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while fill(black):
-        pygame.display.flip()
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit() 
+
+ 
+            screen.fill((0,0,0))
+            pygame.display.flip()
 
 if __name__ == "__main__":
     main()

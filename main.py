40cd98b9301d9b52e25de4eb693e8b4d4068a206
jager-io <jagerwilloughby@gmail.com>
2025-09-19 07:04:21 +0800

    updated .tick() and return

--- main.py
+++ main.py
@@ -15,14 +15,23 @@ def main():
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    
+    clock = pygame.time.Clock()
+    dt = 0 
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit() 
+                return 
 
  
             screen.fill((0,0,0))
             pygame.display.flip()
-
+           
+            dt = clock.tick(60) / 1000
+            print(dt)
+                                                     
 if __name__ == "__main__":
     main()

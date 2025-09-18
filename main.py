27d5191aa846ab2a40b397a5f7e2ecb3fb5a9607
jager-io <jagerwilloughby@gmail.com>
2025-09-18 13:06:22 +0800

    Astroids_Start

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame 
+from constants import *
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+ASTEROID_MIN_RADIUS = 20
+ASTEROID_KINDS = 3
+ASTEROID_SPAWN_RATE = 0.8  # seconds
+ASTEROID_MAX_RADIUS = ASTEROID_MIN_RADIUS * ASTEROID_KINDS
+
+def main():
+    print ("Starting Asteroids!")
+    print (f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while fill(black):
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()

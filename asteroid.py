a45810e45c712b89c619c864f679cbfd07ee0c0f
jager-io <jagerwilloughby@gmail.com>
2025-09-22 07:01:57 +0800

    starting to create asteroids field

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,13 @@
 
-Asteroid(CircleShape):
+
+class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        
+        super().__init__(pygame.Vector2(x, y), radius)
+        self.position = pygame.Vector2(x,y)
+        self.radius = radius
+
+    def draw(self, screen):
+        pygame.draw.circle(screen,"white", self.position, self.radius, width=2)
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame 
 from constants import *
 from player import Player
-
+from asteroidfield import AsteroidField
 
 def main():
     print ("Starting Asteroids!")
@@ -17,10 +17,15 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
-
+    asteroidfield = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    asteroidfield.containers = (updateable,)
+    asteroid_field = AsteroidField()
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
